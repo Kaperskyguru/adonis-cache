93c4a9c9622d2860682bf5e02c7f0f854eb575f8
kaperskyguru <solomoneseme@gmail.com>
2020-03-12 21:46:47 +0100

    Add gitignore

--- src/Contracts/CacheInterface.ts
+++
@@ -1,9 +0,0 @@
-interface CacheInterface {
-  get(name: String): Promise<any>;
-  set(name: String, data: any, duration: Number): Promise<any>;
-  update(name: String, data: any, duration: Number): Promise<any>;
-  delete(name: String): Promise<Boolean>;
-  remember(name: String, duration: Number, callback: Function): Promise<any>;
-  rememberForever(name: String, callback: Function): Promise<any>;
-}
-export default CacheInterface;
