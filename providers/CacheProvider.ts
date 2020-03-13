25aeb127494e78e4a8e33853710c06dacac0efb9
kaperskyguru <solomoneseme@gmail.com>
2020-03-13 02:41:19 +0100

    Remove module exports

--- providers/CacheProvider.ts
+++ providers/CacheProvider.ts
@@ -1,17 +1,17 @@
 "use strict";
 
-import { IocContract } from "@adonisjs/fold";
+const { ServiceProvider } = require("@adonisjs/fold");
 
-export class CacheProvider {
-  constructor(protected $container: IocContract) {}
-
-  register() {
-    this.$container.singleton("Kaperskyguru/Adonis-Cache", () => {
-      const Config = this.$container.use("Adonis/Src/Config");
+class CacheProvider extends ServiceProvider {
+  public register() {
+    this.app.singleton("Kaperskyguru/Adonis-Cache", app => {
+      const Config = app.use("Adonis/Src/Config");
       const CacheLoader = require("../src/CacheLoader");
       return new CacheLoader(Config);
     });
 
-    this.$container.alias("Kaperskyguru/Adonis-Cache", "Cache");
+    this.app.alias("Kaperskyguru/Adonis-Cache", "Cache");
   }
 }
+
+module.exports = CacheProvider;
